dd52ced0b7e2050e720ddebf50c83fb59015366c
Rayyan Khan <rayyan.khan258@gmail.com>
2018-12-18 13:29:05 -0500

    finding a version ai grader will accept

---
+++ lab4/ai grader hates them/chooseMove1.py
@@ -0,0 +1,207 @@
+import sys
+import operator
+
+# change inputs to specifically a board and a token
+
+# assign those variables from sys input
+if len(sys.argv) < 3:
+    print('Missing required input.')
+startboard = sys.argv[1].lower()
+startTkn = sys.argv[2].lower()
+
+# global variables
+NBRS_flips = {} # NBRS_flips = {index: {adjacent indexes}}
+NBRS_moves = {} # NBRS_moves = {index: {adjacent indexes that moves can be made from}}
+SUBSETS = [] # SUBSETS = [{nbr: [indexes in subset], nbr: [indexes in subset]}, {etc...}]
+TKNSETS = {'o': {i for i in range(64) if startboard[i] == 'o'} - {0, 7, 56, 63},
+             'x': {i for i in range(64) if startboard[i] == 'x'} - {0, 7, 56, 63}} # set of indexes containing o and x
+
+
+# setting up NBRS -- part 1
+idxs = [i for i in range(0, len(startboard))]
+for index in idxs: # make better later if time/energy/if its worth it
+    if index % 8 == 0: # if its on left edge, don't include anything left
+        NBRS_flips[index] = {index + 1,
+                             index - 8, index + 8,
+                             index - 7,
+                             index + 9}\
+            .intersection(idxs) # don't include indexes that don't exist
+    elif index % 8 == 7: # if its on right edge, don't include anything right
+        NBRS_flips[index] = {index - 1,
+                             index - 8, index + 8,
+                             index + 7,
+                             index - 9} \
+            .intersection(idxs)
+    else:
+        NBRS_flips[index] = {index - 1,index + 1,
+                             index - 8, index + 8,
+                             index - 7, index + 7,
+                             index - 9, index + 9} \
+            .intersection(idxs)
+
+
+# setting up SUBSETS
+for index in idxs: # want a dict for each index
+    subDict = {nbr: [] for nbr in NBRS_flips[index]}
+    for nbr in NBRS_flips[index]: # want a key for each neighbor
+        # want the value to be a list of the other indexes in the same
+        # row/column/diagonal, depending on the relationship between the
+        # index and neighbor (which determines whether you're looking at
+        # diagonals, columns, or rows)
+        diff = index - nbr
+        prev = nbr
+        current = nbr + diff
+        while -1 < current < 64 and current in NBRS_flips[prev]:
+            if current != index:
+                subDict[nbr].append(current)
+            prev = current
+            current = current + diff
+        if len(subDict[nbr]) == 0:
+            del subDict[nbr]
+    SUBSETS.append(subDict)
+
+
+# taking out NBRS that moves can't be made from
+NBRS_moves = {index: {key for key in SUBSETS[index]} for index in NBRS_flips}
+delInds = {key for key in NBRS_moves if len(NBRS_moves[key]) == 0}
+for key in delInds:
+    del NBRS_moves[key]
+
+# helper methods
+
+def getScore(board):
+    return board.count('x'), board.count('o')
+
+
+def nextTokens(board): # assuming no passes
+    if board.count('.') % 2: # do better later
+        return 'o', 'x' # next token, token after
+    return 'x', 'o'
+
+
+def getOppToken(token):
+    if token == 'x':
+        return 'o'
+    return 'x'
+
+
+def printBoard(board):
+    for i in range(0, 64, 8):
+        print(' '.join(board[i:i+8]))
+
+
+def printPossMoves(board, possMoves):
+    printBoard(''.join([ch if idx not in possMoves
+                        else '*' for idx, ch in enumerate(board)]))
+
+
+def checkBracketing(token, possInd, adjInd, board):
+    # your token, possible placement index, adjacent index of opponent, current board
+    # return true or false depending on whether your token would
+    # form a bracket with a matching token if placed next to index
+    # also try to think of better ways to go about this
+
+    subset = SUBSETS[adjInd][possInd]
+
+    for index in subset:
+        if board[index] == '.':
+            # if you run into an empty space before bracketing token
+            # then it doesn't work
+            return -1
+        elif board[index] == token:
+            # if you find a bracketing token somewhere along the line
+            # then it does form a bracket
+            return index # return the ending index
+    # if you get through the entire subset and don't find a bracketing token
+    # then too bad
+    return -1
+
+
+def nextMoves(board, tokens = ''):
+    possMoves = set() # {indexes that given/default token may make a move at}
+
+    if tokens == '': # if token isn't given
+        token, oppToken = nextTokens(board) # assume no passes and find next token
+    else:
+        token, oppToken = tokens, getOppToken(tokens)
+
+    for idx in TKNSETS[oppToken]: # check opposing token indexes
+        for nbr in NBRS_moves[idx]: # check if there are spaces you can move into
+            if board[nbr] == '.':
+                if checkBracketing(token, nbr, idx, board) != -1:
+                    # if placing here check whether there's another
+                    # token down the line it would form a bracket with
+                    possMoves.add(nbr) # if so it's a possible move
+    # if len(possMoves) == 0, then nextMoves[0] == False
+    return len(possMoves), possMoves
+
+
+def makeFlips(board, token, position):
+    oppToken = getOppToken(token)
+
+    adjOpps = {nbr for nbr in NBRS_flips[position]
+               if board[nbr] == oppToken and position in SUBSETS[nbr]}
+
+    for opp in adjOpps: # do better later
+        idx = checkBracketing(token, position, opp, board) # maybe pass in idx rather than re-finding
+        if idx > -1:
+            subset = SUBSETS[opp][position]
+            changes = set(subset[:subset.index(idx) + 1] + [position, opp])
+            TKNSETS[token] = TKNSETS[token].union(changes) - {0, 7, 56, 63}
+            TKNSETS[oppToken] = TKNSETS[oppToken] - changes
+            board = ''.join([ch if ind not in changes else token for ind, ch in enumerate(board)])
+    return board, changes
+
+
+def corners(moveSet):
+    return moveSet.intersection({0, 7, 56,  63})
+
+
+def gamePhase(board):
+    numMoves = 64 - board.count('.')
+    if numMoves < 15:
+        return 'B' # beginning
+    elif numMoves < 35:
+        return 'M' # middle
+    else:
+        return 'E' # end
+
+
+def bSortMoves(board, token, possMoves):
+    sortedMoves = []
+    for move in possMoves:
+        flippedBoard, changes = makeFlips(board, token, move)
+        sortedMoves.append((len(changes), move))
+    return sorted(sortedMoves, reverse=True)
+
+def mSortMoves(board, token, possMoves):
+    sortedMoves = []
+    for move in possMoves:
+        flippedBoard, changes = makeFlips(board, token, move)
+        if move in {0, 7, 56, 63}:
+            sortedMoves.append((move, len(changes), 1))
+        else:
+            sortedMoves.append((move, len(changes), 0))
+    return sorted(sortedMoves, key=operator.itemgetter(2))
+
+def eSortMoves(board, token, possMoves):
+    sortedMoves = []
+    for move in possMoves:
+        flippedBoard, changes = makeFlips(board, token, move)
+        if move in {0, 7, 56, 63}:
+            sortedMoves.append((move, len(changes), 1))
+        else:
+            sortedMoves.append((move, len(changes), 0))
+    return sorted(sortedMoves, key=operator.itemgetter(2), reverse=True)
+
+
+# run
+canMove, possMoves = nextMoves(startboard, startTkn)
+oppTkn = getOppToken(startTkn)
+phase = gamePhase(startboard)
+if phase == 'B':
+    print([tp[1] for tp in bSortMoves(startboard, startTkn, possMoves)])
+elif phase == 'M':
+    print([tp[1] for tp in mSortMoves(startboard, startTkn, possMoves)])
+else:
+    print([tp[1] for tp in eSortMoves(startboard, startTkn, possMoves)])
